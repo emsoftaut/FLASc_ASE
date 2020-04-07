53fdfd720fc80ae3f9540ff320e63ac45c015404
chandanNapster <chandan202.alive@gmail.com>
2020-04-07 20:21:18 +1200

    DiagramNodes and Edges
    This code populated two lists DiagramNodeList and DiagramEdgeList

--- HTMLneo4j/src/main/java/com/asemonash/htmlparser/HtmlParser.java
+++ HTMLneo4j/src/main/java/com/asemonash/htmlparser/HtmlParser.java
@@ -2,13 +2,14 @@ package com.asemonash.htmlparser;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Attribute;
 import org.jsoup.nodes.Attributes;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-import org.w3c.dom.css.ElementCSSInlineStyle;
 
 
 public class HtmlParser<E> {
@@ -16,6 +17,10 @@ public class HtmlParser<E> {
 	private File htmlFile;
 	private Document document;
 	private DiagramNode<E> diagramNode;
+	private DiagramEdge<E> diagramEdge;
+	private boolean isNode;
+	private List<DiagramNode<E>> diagramNodesList;
+	private List<DiagramEdge<E>> diagramEdgesList;
 	/**
 	 * afterCounter and beforeCounter are debug
 	 * statements. Remove in the final version
@@ -24,6 +29,8 @@ public class HtmlParser<E> {
 	
 	public HtmlParser(File file) {
 		this.htmlFile = file;
+		diagramNodesList = new LinkedList<DiagramNode<E>>();
+		diagramEdgesList = new LinkedList<DiagramEdge<E>>();
 	}
 	
 	public void initHtmlParser() {
@@ -35,32 +42,56 @@ public class HtmlParser<E> {
 			Elements areaTagElements = mapHtmlDoc.getElementsByTag("area");
 
 			for(Element areaTagElement : areaTagElements) {
+				diagramNode = new DiagramNode<E>();
+				diagramEdge = new DiagramEdge<E>();
+				//isNode = true;
 				extractDataFrmAttr(areaTagElement.attributes());
-				System.out.println("************");
+				if(isNode) {
+					diagramNodesList.add(diagramNode);
+				}
+				else {
+					diagramEdgesList.add(diagramEdge);
+				}
+				//System.out.println("************");
+//				System.out.println("NODE "+diagramNode);
+//				System.out.println("EDGE "+diagramEdge);
 				beforeCounter++;
 			}
 			
-			rowCounterDebugFunc();
+			//rowCounterDebugFunc();
+			displayNodeList();
+			System.out.println("******************");
+			displayEdgeList();
+			
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 
 	private void extractDataFrmAttr(Attributes attributes) {
-		diagramNode = new DiagramNode<E>();
+	
+		
 		
 		for(Attribute attribute: attributes.asList()) {
-			//System.out.println(attribute.getKey() +"::"+ attribute.getValue());
+
 			if(attribute.getKey().equalsIgnoreCase("alt") && 
 				(attribute.getValue().equalsIgnoreCase("from")|| 
 						attribute.getValue().equalsIgnoreCase("to"))){
-					System.out.println("Edge");
+					if(attribute.getValue().equalsIgnoreCase("from")) {
+						diagramEdge.setLabel(Label.FROM);
+					}
+					else {
+						diagramEdge.setLabel(Label.TO);
+					}
+					isNode = false;
 			}
+			
 			else if(attribute.getKey().equalsIgnoreCase("alt") && 
 					(attribute.getValue().toLowerCase().contains("CONDITION".toLowerCase())|| 
 							attribute.getValue().toLowerCase().contains("OPERATION".toLowerCase()))) {
-				System.out.println("CONDITION/OPERATION-->"+ attribute.getValue());
+				isNode = true;
 				if(attribute.getValue().toLowerCase().contains("OPERATION".toLowerCase())) {
+					
 					diagramNode.setLabel(Label.OPERATION);
 					diagramNode.setName((E) attribute.getValue());
 				}
@@ -69,10 +100,12 @@ public class HtmlParser<E> {
 					diagramNode.setName((E) attribute.getValue());
 				}
 			}
+			
 			else if(attribute.getKey().equalsIgnoreCase("alt") &&
 					attribute.getValue().toLowerCase().contains("INITIAL STEP".toLowerCase()) ||
 					attribute.getValue().toLowerCase().contains("STEPS".toLowerCase())) {
-				System.out.println("INITIAL STEP-->" + attribute.getValue());
+				//System.out.println("INITIAL STEP-->" + attribute.getValue());
+				isNode = true;
 				if(attribute.getValue().toLowerCase().contains("INITIAL STEP".toLowerCase())) {
 					diagramNode.setLabel(Label.INITIAL_STEP);
 					diagramNode.setName((E) attribute.getValue());
@@ -84,21 +117,32 @@ public class HtmlParser<E> {
 			}
 			else if(attribute.getKey().equalsIgnoreCase("alt") &&
 					attribute.getValue().toLowerCase().contains("TASK".toLowerCase())) {
-				System.out.println("TASKS-->" + attribute.getValue());
+				//System.out.println("TASKS-->" + attribute.getValue());
+				isNode = true;
 				diagramNode.setLabel(Label.TASK);
 				diagramNode.setName((E)attribute.getValue());
 			}
 			else if(attribute.getKey().equalsIgnoreCase("alt")) {
-				System.out.println("SUB TASKS-->" + attribute.getValue());
+				//System.out.println("SUB TASKS-->" + attribute.getValue());
+				isNode = true;
 				diagramNode.setLabel(Label.SUB_TASK);
 				diagramNode.setName((E)attribute.getValue());
 			}
 			
 			if(attribute.getKey().equalsIgnoreCase("href")) {
-				String nodeID = attribute.getValue().substring(1);
-				System.out.println("ID-->" + nodeID);
+				if(isNode == true) {
+					String nodeID = attribute.getValue().substring(1);
+					diagramNode.setId((E) nodeID);
+					//System.out.println("Node-->" + diagramNode);
+				}
+				else {
+					String edgeID = attribute.getValue().substring(1);
+					diagramEdge.setId((E) edgeID);
+					//System.out.println("Edge-->" + diagramEdge);
+				}
 			}
 		}
+		//isNode = true;
 		afterCounter++;
 		
 		//System.out.println(it.getAttributes());
@@ -116,5 +160,16 @@ public class HtmlParser<E> {
 		System.out.println("******************");
 		System.out.println("Rows after filtering-->"+ afterCounter);
 	}
+	
+	public void displayNodeList() {
+		for(DiagramNode<E> nodes: diagramNodesList) {
+			System.out.println(nodes);
+		}
+	}
  	
+	public void displayEdgeList() {
+		for(DiagramEdge<E> edges: diagramEdgesList) {
+			System.out.println(edges);
+		}
+	}
 }
