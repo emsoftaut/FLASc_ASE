1f9b39d2afebab7ee5b10143ea733b3303d860f6
chandanNapster <chandan202.alive@gmail.com>
2020-04-14 20:26:26 +1200

    Update RelationshipLinkedSet
    Created a class which mimiks a set.
    This is done to remove duplicate nodes comming from the HTML parser class into the CypherQueryBuilder class

--- HTMLneo4j/src/main/java/com/asemonash/htmlparser/RelationshipLinkedSet.java
+++ HTMLneo4j/src/main/java/com/asemonash/htmlparser/RelationshipLinkedSet.java
@@ -3,38 +3,42 @@ package com.asemonash.htmlparser;
 import java.util.AbstractSet;
 import java.util.Iterator;
 
-public class RelationshipLinkedSet<E extends Comparable> extends AbstractSet<E> {
+public class RelationshipLinkedSet<E extends Comparable> {
 
-	private Node<E> currentNode;
 	private Node<E> firstNode;
 	private int numOfElements;
 	
 	public RelationshipLinkedSet() {
-		// TODO Auto-generated constructor stub
 		firstNode = null;
-		currentNode = null;
 		numOfElements = 0;
 	}
 	
-	public void add(E startNode, E endNode) {
+	private void add(E startNode, E endNode) {
 		Node newNode = new Node<E>(startNode, endNode);
-		if(firstNode == null) {
-			
-		}
-	}
-	
-	@Override
-	public Iterator<E> iterator() {
-		// TODO Auto-generated method stub
-		return null;
+		newNode.next = firstNode;
+		firstNode = newNode;
+		numOfElements++;
 	}
 
-	@Override
 	public int size() {
 		return this.numOfElements;
 	}
 	
-	public boolean containsRelationshipElements(E start, E end) {
+	public void addtoSet(E start, E end) {
+		Node<E> currentNode = firstNode;
+		int counter = 0;
+		while(currentNode != null) {
+			if(containsRelationshipElements(currentNode, start, end)) {
+				counter++;
+			}	
+			currentNode = currentNode.next;
+		}
+		if(counter == 0){
+			add(start, end);
+		}
+	}
+	
+	private boolean containsRelationshipElements(Node<E> currentNode, E start, E end) {
 		if(currentNode.startNode.compareTo(start) == 0 
 				&& currentNode.endNode.compareTo(end)== 0)
 			return true;
@@ -53,4 +57,40 @@ public class RelationshipLinkedSet<E extends Comparable> extends AbstractSet<E> 
 			this.next = null;
 		}
 	}
+	
+	public void display() {
+		Node<E> currentNode = firstNode;
+		while(currentNode != null) {
+			System.out.println(currentNode.startNode +"::"+ currentNode.endNode);
+			currentNode = currentNode.next;
+		}
+		System.out.println("***************");
+		
+	}
+	
+	public static void main(String[] arg) {
+		RelationshipLinkedSet relationshipLinkedSet = new RelationshipLinkedSet<Comparable>();
+		relationshipLinkedSet.addtoSet("1_2", "1_3");
+		relationshipLinkedSet.addtoSet("3_2", "3_3");
+		relationshipLinkedSet.addtoSet("1_2", "1_3");
+		
+		relationshipLinkedSet.addtoSet("2_2", "2_3");
+		relationshipLinkedSet.addtoSet("2_2", "2_3");
+		relationshipLinkedSet.addtoSet("3_2", "3_3");
+		relationshipLinkedSet.addtoSet("3_2", "3_3");
+		relationshipLinkedSet.addtoSet("3_2", "3_3");
+		relationshipLinkedSet.addtoSet("3_2", "3_3");
+		relationshipLinkedSet.addtoSet("2_2", "2_3");
+		relationshipLinkedSet.addtoSet("1_2", "1_3");
+		relationshipLinkedSet.addtoSet("1_2", "1_3");
+		relationshipLinkedSet.addtoSet("3_2", "1_4");
+		relationshipLinkedSet.addtoSet("3_2", "1_4");
+		relationshipLinkedSet.addtoSet("3_2", "1_4");
+		relationshipLinkedSet.addtoSet("chandan", "sudha");
+		relationshipLinkedSet.addtoSet("chandan", "sudha");
+		//System.out.println(relationshipLinkedSet.size());
+		
+		relationshipLinkedSet.display();
+	}
+	
 }
